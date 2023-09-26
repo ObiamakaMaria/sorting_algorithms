11d42a424a7aef6ec0a1b687b4997b01572689a9
memmalino <memmalino@gmail.com>
2023-09-26 00:42:20 +0000

    correction

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,83 +1,76 @@
 #include "sort.h"
 
-void quicksort(int *array, int low, int high);
-void quick_sort(int *array, size_t size);
+/*prototypes. funcs() defined later in this file*/
+void sort_alg(int *arr, int left, int right, size_t size);
+int split(int *arr, int left, int right, size_t size);
 
 /**
- * lomuto_partition - This function uses the Lomuto partition
- * scheme for Quick Sort
- *
- * @array: Pointer to the array to be partitioned
- * @low: Index of the lower bound of the partition
- * @high: Index of the upper bound of the partition
- *
- * Return: Index of the pivot element
- */
-int lomuto_partition(int *array, int low, int high)
-{
-	int i, j;
-	int temp, pivot;
-
-	pivot = array[high];
-	i = low - 1;
-
-	for (j = low; j < high; j++)
-	{
-		if (array[j] <= pivot)
-		{
-			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-		}
-	}
-
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
-
-	return (i + 1);
-}
-
-/**
- * quicksort - This function uses a recursive function to
- * perform Quick Sort
- *
- * @array: Pointer to the array to be sorted
- * @low: Index of the lower bound of the partition
- * @high: Index of the upper bound of the partition
- *
- */
-void quicksort(int *array, int low, int high)
-{
-	int pivot_idx;
-
-	if (low < high)
-	{
-		pivot_idx = lomuto_partition(array, low, high);
-
-		print_array(array, high - low + 1);  /* Print the subarray being sorted */
-
-		quicksort(array, low, pivot_idx - 1);
-		quicksort(array, pivot_idx + 1, high);
-	}
-}
-
-/**
- * quick_sort - This function sorts an array of integers in
- * ascending order using the Quick Sort algorithm with
- * Lomuto partition scheme.
- *
- * @array: Pointer to the array to be sorted
- *
- * @size: Size of the array
+ * quick_sort - quicksort algorithm
+ * @array: array to be sorted
+ * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size <= 1)
 		return;
+	sort_alg(array, 0, size - 1, size);
+}
 
-	quicksort(array, 0, size - 1);
+/**
+ * sort_alg - recursive sorting algorithm
+ * @arr: array
+ * @left: leftmost index
+ * @right: rightmost index
+ * @size: full size of array
+ */
+void sort_alg(int *arr, int left, int right, size_t size)
+{
+	int pivot;
 
-	print_array(array, size);  /* Print the entire sorted array */
+	if (left < right)
+	{
+		pivot = split(arr, left, right, size);
+		sort_alg(arr, left, pivot - 1, size);
+		sort_alg(arr, pivot + 1, right, size);
+	}
+}
+
+/**
+ * split - split array
+ * @arr: array
+ * @left: leftmost index
+ * @right: rightmost index
+ * @size: full array size
+ * Return: pivot index
+ */
+int split(int *arr, int left, int right, size_t size)
+{
+	int i, i2, pivot, tmp;
+
+	pivot = arr[right];
+	i = left;
+
+	for (i2 = left; i2 < right; i2++)
+	{
+		if (arr[i2] < pivot)
+		{
+			if (i != i2)
+			{
+				tmp = arr[i2];
+				arr[i2] = arr[i];
+				arr[i] = tmp;
+				print_array(arr, size);
+			}
+			i++;
+		}
+	}
+	if (arr[i] != arr[right])
+	{
+		tmp = arr[i];
+		arr[i] = arr[right];
+		arr[right] = tmp;
+		print_array(arr, size);
+	}
+
+	return (i);
 }
